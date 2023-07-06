704aa72542ac536b1d4e3c33ae4a56ee213d73dc
BSK-98 <nathaniellee20graham@gmail.com>
2023-07-06 12:05:05 +0200

    add user Avater

--- src/components/global/Map.jsx
+++ src/components/global/Map.jsx
@@ -1,20 +1,17 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
 const Map = () => {
-    const position = [-15.357609,29.1653092]
+    const position = [-15.357609, 29.1653092]
     return (
-        <MapContainer center={position} zoom={9} scrollWheelZoom={false}>
+        <MapContainer center={position} zoom={9} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright"></a> '
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {/* <TileLayer
-                attribution='&copy; <a href="https://www.zi-guide">Zi Guide</a>'
-                
-            /> */}
+
             <Marker position={position}>
                 <Popup>
-                    Province Name
+                    Lusaka Province
                 </Popup>
             </Marker>
         </MapContainer>
